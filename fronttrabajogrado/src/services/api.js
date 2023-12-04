9bcb90bd9d1e5a3139ef1700069d2c5554f37cb1
djvalderrama67 <100389591+djvalderrama67@users.noreply.github.com>
2023-12-03 23:55:57 -0500

    Update api.js

--- fronttrabajogrado/src/services/api.js
+++ fronttrabajogrado/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://back-trasteat-bbb5675bd933.herokuapp.com';
 
 export default axios.create({
     baseURL: API_URL,
